dff75e8bb4005ce1dde5275ecea4c3fdd73f68f9
erzhan <kubatov_e@auca.kg>
2022-02-01 17:57:32 +0600

    Initial Commit

---
+++ StopTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StopTimer
+//
+//  Created by Kubatov Erzhan on 01.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
